b90884bdfb78d74af43d00532af5ebce6f8900bb
Julian Kunaschik <juliankunaschik@MacBook-Air-de-Julian.local>
2025-06-05 17:02:26 -0300

    add HearFilledIcon

---
+++ app/shared/ui/atoms/HeartFilledIcon.tsx
@@ -0,0 +1,12 @@
+import React from "react";
+
+export const HeartFilledIcon: React.FC<React.SVGProps<SVGSVGElement>> = (props) => (
+  <svg
+    {...props}
+    viewBox="0 0 24 24"
+    fill="currentColor"
+    xmlns="http://www.w3.org/2000/svg"
+  >
+    <path d="M12 21.35l-1.45-1.32C5.4 15.36 2 12.28 2 8.5 2 6 4 4 6.5 4c1.74 0 3.41 1.01 4.13 2.44h1.74C14.09 5.01 15.76 4 17.5 4 20 4 22 6 22 8.5c0 3.78-3.4 6.86-8.55 11.54L12 21.35z" />
+  </svg>
+);
