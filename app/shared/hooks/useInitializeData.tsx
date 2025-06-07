f674d1219b56a9e37e05e99fae27d49a72c804f7
Julian Kunaschik <juliankunaschik@gmail.com>
2025-06-07 13:12:50 -0300

    improve useInitializeData hook

--- app/shared/hooks/useInitializeData.tsx
+++ app/shared/hooks/useInitializeData.tsx
@@ -7,24 +7,28 @@ import { useCategoryStore } from "@store/categoryStore";
 export function useInitializeData() {
   const { getCategoriesQuery } = useGetCategories();
   const { getPhrasesQuery } = useGetPhrases();
+
+  const phrases = usePhraseStore((state) => state.phrases);
   const setPhrases = usePhraseStore((state) => state.setPhrases);
+
+  const categories = useCategoryStore((state) => state.categories);
   const setCategories = useCategoryStore((state) => state.setCategories);
 
   useEffect(() => {
-    if (getPhrasesQuery.data) {
+    if (getPhrasesQuery.data && phrases.length === 0) {
       const phrasesWithDate = getPhrasesQuery.data.map((phrase) => ({
         ...phrase,
         date: new Date(phrase.date),
       }));
       setPhrases(phrasesWithDate);
     }
-  }, [getPhrasesQuery.data, setPhrases]);
+  }, [getPhrasesQuery.data, phrases.length, setPhrases]);
 
   useEffect(() => {
-    if (getCategoriesQuery.data) {
+    if (getCategoriesQuery.data && categories.length === 0) {
       setCategories(getCategoriesQuery.data);
     }
-  }, [getCategoriesQuery.data, setCategories]);
+  }, [getCategoriesQuery.data, categories.length, setCategories]);
 
   return {
     isLoading: getCategoriesQuery.isLoading || getPhrasesQuery.isLoading,
